55f0d439a5afa6b20fb8ae7e817a8aab94d19cc9
Outvi V <olvi.git@mailnull.com>
2018-12-08 02:04:15 +0800

    Add unittest grader

--- dmoj/graders/__init__.py
+++ dmoj/graders/__init__.py
@@ -3,3 +3,4 @@ from dmoj.graders.custom import CustomGrader
 from dmoj.graders.interactive import InteractiveGrader
 from dmoj.graders.signature import SignatureGrader
 from dmoj.graders.standard import StandardGrader
+from dmoj.graders.unittest import UnitTestGrader
