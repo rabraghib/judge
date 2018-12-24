cdcbdce67a7b1fbc70137ea8ba9a65aa2bb8589d
schrodingerzhu <jimmyzhu1999@protonmail.com>
2018-12-24 11:57:47 +0000

    fix julia msync syscall failure

--- dmoj/executors/JULIA.py
+++ dmoj/executors/JULIA.py
@@ -9,7 +9,7 @@ class Executor(ScriptExecutor):
     syscalls = ['epoll_ctl', 'epoll_create', 'epoll_wait', 'poll',                                                                                                                          
                 'epoll_create1', 'pipe2', 'eventfd2', 'rt_sigtimedwait',                                                                                                                    
                 'sched_getaffinity', 'sched_setaffinity', 'mbind',                                                                                                                          
-                'mincore', 'memfd_create', 'pwrite64']                                                                                                                                                                  
+                'mincore', 'memfd_create', 'pwrite64', 'msync']                                                                                                                                                                  
     fs = ['/opt/julia-1\.0\.1/.*', '/home/judge/\.julia/.*', '']                                                                                                                                                                                            
     address_grace = 131072 << 2                                                                                                                                                             
     nproc = -1                                                                                                                                                                            
